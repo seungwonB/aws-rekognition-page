4d0d34f943266d38608f2339a4cc724858832827
Seungwon Bang <73030613+seungwonB@users.noreply.github.com>
2021-12-03 00:32:22 +0900

    add: detect_faces 출력

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
-import boto3
 from pymongo import MongoClient
+import boto3
 client = MongoClient('localhost', 27017) # 내 거
 # client = MongoClient('mongodb://test:test@localhost', 27017) # 올릴 때
 db = client.dbsparta_plust_week1
-
+from collections import Counter
+from detect_faces import *
 from datetime import datetime
 
 @app.route('/')
 def home():
-    account_id = boto3.client('sts').get_caller_identity().get('Account')
-    return render_template('index.html', accid=account_id)
+    return render_template('index.html')
 
 @app.route('/diary', methods=['GET'])
 def show_diary():
@@ -43,9 +43,18 @@ def save_diary():
     }
 
     db.diary.insert_one(doc)
-    return jsonify({'msg': '저장 완료!'})
 
+    return jsonify({'msg': doc['file']})
 
+@app.route('/reko', methods=['POST'])
+def rekognition():
+    content_receive = request.form['content_give']
+    file_receive = request.form['file_give']
+
+    res = photo_analysis(file_receive, content_receive)
+
+    # return jsonify({'msg': f'{content_receive}, {file_receive}'})
+    return jsonify({'msg': f'{res}'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
