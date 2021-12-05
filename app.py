e45821baf3724f82540093f8361e29264cea1d4e
Seungwon Bang <73030613+seungwonB@users.noreply.github.com>
2021-12-05 21:33:34 +0900

    add: text 추출 API 추가

--- app.py
+++ app.py
@@ -4,47 +4,15 @@ from pymongo import MongoClient
 import boto3
 client = MongoClient('localhost', 27017) # 내 거
 # client = MongoClient('mongodb://test:test@localhost', 27017) # 올릴 때
-db = client.dbsparta_plust_week1
+# db = client.dbrekognition
 from collections import Counter
 from detect_faces import *
+from text import *
 from datetime import datetime
 
 @app.route('/')
 def home():
     return render_template('index.html')
-
-@app.route('/diary', methods=['GET'])
-def show_diary():
-    diaries = list(db.diary.find({}, {'_id': False}))
-    return jsonify({'all_diary': diaries})
-
-@app.route('/diary', methods=['POST'])
-def save_diary():
-    title_receive = request.form['title_give']
-    content_receive = request.form['content_give']
-    file = request.files["file_give"]
-
-    extension = file.filename.split(".")[-1]
-
-    today = datetime.now()
-    mytime = today.strftime('%Y-%m-%d-%H-%M-%S') # 올라간 사진 이름
-    mytime2 = today.strftime('%Y.%m.%d') # 올린 사진 날짜 표시
-
-    filename = f'file-{mytime}'
-    timename = f'{mytime2}'
-    save_to = f'static/{filename}.{extension}'
-    file.save(save_to)
-
-    doc = {
-        'title': title_receive,
-        'content': content_receive,
-        'file': f'{filename}.{extension}',
-        'time': f'{timename}'
-    }
-
-    db.diary.insert_one(doc)
-
-    return jsonify({'msg': doc['file']})
 
 @app.route('/reko', methods=['POST'])
 def rekognition():
@@ -53,8 +21,15 @@ def rekognition():
 
     res = photo_analysis(file_receive, content_receive)
 
-    # return jsonify({'msg': f'{content_receive}, {file_receive}'})
     return jsonify({'msg': f'{res}'})
+
+@app.route('/reko_text', methods=['POST'])
+def rekognition_text():
+    content_receive = request.form['content_give']
+    file_receive = request.form['file_give']
+
+    res_text = text_extract(file_receive, content_receive)
+    return jsonify({'msg': f'{res_text}'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
